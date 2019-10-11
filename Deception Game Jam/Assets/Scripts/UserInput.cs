111e5e19487d88dc82a3ed2e85e74175581b76df
Phantasma_ <phantasmalspectre@aol.com>
2019-10-11 19:50:04 -0400

    PlayerController
    Created a player controller, and game manager

---
+++ Deception Game Jam/Assets/Scripts/UserInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    #region References
+    private Rigidbody playerRigidbody;
+    private GameManager instance;
+    #endregion
+    #region Variables
+    [SerializeField] private float playerMovementSpeed;
+    [SerializeField] private float turnSensitivity;
+    #endregion
+    private void Start()
+    {
+        instance = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+        playerRigidbody = instance.player.GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        Keypress();
+        PlayerMovement();
+    }
+    void Keypress()
+    {
+
+    }
+    void PlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 velX = instance.player.transform.right * horizontal;
+        Vector3 velZ = instance.player.transform.forward * vertical;
+        Vector3 vel = (velX + velZ).normalized * playerMovementSpeed;
+        vel.y = playerRigidbody.velocity.y;
+        playerRigidbody.velocity = vel;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        Vector3 rot = instance.player.transform.eulerAngles;
+        rot.y += mouseX * turnSensitivity;
+        instance.player.transform.eulerAngles = rot;
+
+    }
+}
