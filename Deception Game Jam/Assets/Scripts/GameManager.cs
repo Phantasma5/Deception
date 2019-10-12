983d3c8e6c528c0ea3b08a9b83c24f3fcaf73e67
niovanna2 <54962609+niovanna2@users.noreply.github.com>
2019-10-11 23:34:58 -0400

    Nav mesh stuff

--- Deception Game Jam/Assets/Scripts/GameManager.cs
+++ Deception Game Jam/Assets/Scripts/GameManager.cs
@@ -6,7 +6,10 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject player;
+    public GameObject hixboxPrefab;
+    [SerializeField] private GameObject wolfForm;
     [HideInInspector] public List<GameObject> myPersons = new List<GameObject>();
+    [HideInInspector] public bool transformed = false;
     private void Awake()
     {
         if(null != instance)
@@ -15,5 +18,20 @@ public class GameManager : MonoBehaviour
         }
         instance = this;
         DontDestroyOnLoad(this.gameObject);
+    }
+    private void Update()
+    {
+        if(Time.time > 2 && !transformed)
+        {
+            transformed = true;
+            Transformation();
+        }
+    }
+
+    public void Transformation()
+    {
+        GameObject temp = Instantiate(wolfForm, player.transform.position, player.transform.rotation);
+        Destroy(player.gameObject);
+        player = temp;
     }
 }
