4827cbdb0cba284bb536a458e188ab15eed9fa7f
Phantasma_ <phantasmalspectre@aol.com>
2019-10-11 20:33:28 -0400

    Camera Controller

---
+++ Deception Game Jam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameManager instance;
+    private Vector3 modifier;
+    [SerializeField] private float followDistance;
+    [SerializeField] private float followHeight;
+    [SerializeField] private float followSpeed;
+    private void Start()
+    {
+        instance = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+    }
+    private void Update()
+    {
+        //creating follow modifier
+        modifier = instance.player.transform.forward * -followDistance;
+        modifier.y = followHeight;
+
+        transform.position = instance.player.transform.position + modifier;
+        Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, transform.position, followSpeed);
+        Camera.main.transform.LookAt(instance.player.transform.position);
+    }
+}
