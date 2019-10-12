a30afe48a6f4b32fded494405772a12b8d52740b
Phantasma_ <phantasmalspectre@aol.com>
2019-10-12 15:37:47 -0400

    Sunset
    added sunset to be viewed in full scene

---
+++ Deception Game Jam/Assets/Scripts/SingleUse/Sunset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sunset : MonoBehaviour
+{
+    private GameManager instance;
+    private float startTime;
+    private void Start()
+    {
+        instance = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+        startTime = instance.talkTimer;
+    }
+    private void Update()
+    {
+        if(instance.talkTimer > 0)
+        {
+            transform.Rotate(new Vector3(0.08f, 0, 0));
+        }
+    }
+
+}
