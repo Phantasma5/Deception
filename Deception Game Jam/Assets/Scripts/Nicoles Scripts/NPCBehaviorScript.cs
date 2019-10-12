af2e1305fd93d2909f8ccc855eba7bdeb2bda04b
niovanna2 <54962609+niovanna2@users.noreply.github.com>
2019-10-11 20:21:44 -0400

    NPC Speak

---
+++ Deception Game Jam/Assets/Scripts/Nicoles Scripts/NPCBehaviorScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCBehaviorScript : MonoBehaviour
+{
+    [SerializeField]
+    private int startTalk;
+    public string playerTalk;
+    public Text textBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTalk = Random.Range(0, 2);
+        if (startTalk == 0)
+        {
+            playerTalk = "Yeehaw! Howdy partner!";
+        }
+        else if (startTalk == 1)
+        {
+            playerTalk = "Hi, hows it going?";
+        }
+        else if (startTalk == 2)
+        {
+            playerTalk = "Henlo!";
+        }
+
+        StartCoroutine("PlayerIsSpeaking");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator PlayerIsSpeaking()
+    {
+        textBox.text = playerTalk;
+        Debug.Log(textBox.text);
+        yield return new WaitForSeconds(5f);
+        textBox.text = "";
+    }
+}
