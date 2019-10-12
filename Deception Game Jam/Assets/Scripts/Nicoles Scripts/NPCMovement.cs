9bd723eb3be99c69dca4addd3b7feb70f3555638
niovanna2 <54962609+niovanna2@users.noreply.github.com>
2019-10-12 09:11:39 -0400

    Npc movement

--- Deception Game Jam/Assets/Scripts/Nicoles Scripts/NPCMovement.cs
+++ Deception Game Jam/Assets/Scripts/Nicoles Scripts/NPCMovement.cs
@@ -5,20 +5,36 @@ using UnityEngine.AI;
 
 public class NPCMovement : MonoBehaviour
 {
-    NavMeshAgent npcAgent;
+    public Transform[] points;
     public Transform target;
     Vector3 moveTo;
+    NavMeshAgent npcAgent;
+    int destinationPoint;
 
     // Start is called before the first frame update
     void Start()
     {
         npcAgent = GetComponent<NavMeshAgent>();
+        npcAgent.autoBraking = false;
+        NextPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveTo = target.position;
-        npcAgent.destination = moveTo;
+        if(!npcAgent.pathPending && npcAgent.remainingDistance < 5.0f)
+        {
+            NextPoint();
+        }
+    }
+
+    void NextPoint()
+    {
+        if(points.Length == 0)
+        {
+            return;
+        }
+        npcAgent.destination = points[destinationPoint].position;
+        destinationPoint = (destinationPoint + 1) % points.Length;
     }
 }
